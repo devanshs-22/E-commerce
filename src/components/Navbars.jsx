ef9fefce1fb8ee1a9a27e215b9b84f9e28de2bd3
YashChauhan7060 <yc230310@gmail.com>
2025-03-25 00:02:39 +0530

    initial file structure

---
+++ src/components/Navbars.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    height:60px;
+`
+
+const Navbars = () => {
+  return (
+    <Container>
+      navbar
+    </Container>
+  )
+}
+
+export default Navbars
